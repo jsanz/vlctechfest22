2870d07667f36edba33362ca97cb2d7d9afd26be
Jorge Sanz <jorge@jorgesanz.net>
2022-06-15 09:44:35 +0200

    changed theme, added markdown support

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import Reveal from 'reveal.js';
 import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
+import slidesText from 'bundle-text:./slides.md';
 
-let deck = new Reveal({
-   plugins: [ Markdown ]
-})
-deck.initialize();
+
+document.addEventListener('DOMContentLoaded', function(){
+   let slidesTemplate = document.getElementById('slides-content');
+   slidesTemplate.textContent = slidesText;
+
+   let deck = new Reveal({
+      slideNumber: 'c/t',
+      hashOneBasedIndex: true,
+      hash: true,
+      loop: true,
+      transition: 'fade',
+      showNotes: false,
+      plugins: [ Markdown ]
+   })
+   deck.initialize();
+});
---
+++ src/slides.md
@@ -0,0 +1,19 @@
+# Análisis y visualización de la erupción del Cumbre Vieja
+
+VLCTechFest | 2022-06-18 | Valencia
+
+Note:
+
+Diapositiva de espera para arrancar la charla
+
+---
+
+## Jorge Sanz
+
+Note:
+
+Me presento
+
+---
+
+## Slide 3
